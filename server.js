dc940834b23e1eb23ea76f925417b0bc39760860
senam-art <senam.dzomeku@ashesi.edu.gh>
2026-04-04 08:59:46 +0000

    dream oceam deloy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`✅ Ashesi Shuttle Backend running on Port ${PORT}`);
